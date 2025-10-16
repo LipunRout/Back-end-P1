7969ef3124489c3432c60a268d9f2015cabe1e65
LIPUN ROUT <lipunrout001@gmail.com>
2025-10-16 20:41:51 +0530

    updated

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-const express=require('express')
-const app=express();
+const express = require("express");
+const app = express();
+const path = require("path");
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true})); 
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine','ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
 
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.get('/',(req,res)=>{
-  res.render('index')
-})
+app.get("/profile/:username", (req, res) => {
+  res.send(`welcome ${req.params.username}`);
+});
+app.get("/profile/:username/:age", (req, res) => {
+  res.send(`welcome ${req.params.username} ${req.params.age}`);
+});
 
-
-app.listen(3000,()=>{
-  console.log('No error')
-})
+app.listen(3000, () => {
+  console.log("No error");
+});
