7c07ee282521477c256e8a2dee2ae49de4af490f
LIPUN ROUT <lipunrout001@gmail.com>
2025-10-17 00:36:29 +0530

    updated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const fs = require("fs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -8,9 +9,16 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  fs.readdir(`/Users/lipunsmac/Desktop/Back-end-P1/files`, (err, files) => {
+    res.render("index", { files: files });
+  });
 });
 
+app.post("/create", (req, res) => {
+  fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.details,(error)=>{
+    res.redirect('/');
+  })
+});
 app.get("/profile/:username", (req, res) => {
   res.send(`welcome ${req.params.username}`);
 });
